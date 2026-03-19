87da69f4c6f9cc58568f5a6e2632179a5f21866d
Anshul036 <91818827+Anshul036@users.noreply.github.com>
2026-03-19 05:47:38 +0000

    Datatypes done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" //treeat all JS code as newer version
+
+// alert(hello)     we are using node js , not browser
+// alert(3+3)
+
+
+console.log(3+3)  
+console.log("Anshul")   // code reuseability should we high
+
+
+let name = "Anshul"
+let age = 20
+let isLoggedIn = false
+
+
+// number  => 2 to power 53
+// big int 
+// String => "" 
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+//
+
+
+
+
+// object
+
+console.log(typeof undefined);      //undefined
+console.log(typeof null);           //object
